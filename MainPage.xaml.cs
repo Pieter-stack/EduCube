41b5d96e2ab4f5983a1f8e7fb4e04de1d804017a
hellodvh <hellodvh@gmail.com>
2022-08-06 13:58:43 +0200

    Add Login Page.  Add images. Update Fonts and Colours.
    Layout and styling of Login page done. Added the logo and background images. Updated the fonts and colour resources.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,6 +7,13 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-
+	async void OnLoginClicked(object sender, EventArgs e)
+	{
+		Button button = (Button)sender;
+		await DisplayAlert("Alert", "Login Button Clicked!", "OK");
+	}
+    //TODO:
+    //1. Email Validation with EmailValidationBehavior ('https://docs.microsoft.com/en-us/dotnet/communitytoolkit/maui/behaviors/email-validation-behavior')
+    //2.Login/Authentication funcationality with Username and Password.
 }
 
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,7 +11,11 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+				fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+				fonts.AddFont("Rukib-Bold.ttf", "RubikBold");
+				fonts.AddFont("Rukib-Regular.ttf", "RubikRegular");
+            });
 
 		return builder.Build();
 	}
