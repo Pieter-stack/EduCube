b2945c23eb56d26291ddd47acfee58762aa2f22b
Pieter-stack <pieterven12@gmail.com>
2022-08-16 13:43:22 +0200

    Fixed new project to have all content of old project
    Dashboard, Funds and Subject page, linked fonts, added images, chats NuGet packages installed, added charts/Updated .gitignore/Fixed files missing, NuGet packages and added Dylan's fix for navbar images not showing on his side of the project to this commit due to bad pull request to main./Fixed files missing, NuGet packages and added Dylan's fix for navbar images not showing on his side of the project to this commit due to bad pull request to main./////// New: Started to add DB functionality , models, ViewModels, Repositories///////////////

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿namespace EduCube;
+
+using EduCube.Models;
+using EduCube.Services;
 using SkiaSharp.Views.Maui.Controls.Hosting;
 
 public static class MauiProgram
@@ -19,6 +22,25 @@ public static class MauiProgram
                 fonts.AddFont("Rubik-Regular.ttf", "Rubik-Regular");
             });
 
-		return builder.Build();
+		//Views
+		builder.Services.AddSingleton<DashboardPage>();
+        builder.Services.AddSingleton<FundsPage>();
+        builder.Services.AddSingleton<StudentsPage>();
+        builder.Services.AddSingleton<SubjectsPage>();
+        builder.Services.AddSingleton<TeachersPage>();
+        //Viewmodels
+
+
+
+        //DB Repos
+        string userDBPath = FileAccessHelper.GetLocalFilePath("educubeDatabase.db3");
+        builder.Services.AddSingleton<AdminRepository>(s => ActivatorUtilities.CreateInstance<AdminRepository>(s, userDBPath));
+        builder.Services.AddSingleton<FundRepository>(s => ActivatorUtilities.CreateInstance<FundRepository>(s, userDBPath));
+        builder.Services.AddSingleton<StaffRepository>(s => ActivatorUtilities.CreateInstance<StaffRepository>(s, userDBPath));
+        builder.Services.AddSingleton<StudentRepository>(s => ActivatorUtilities.CreateInstance<StudentRepository>(s, userDBPath));
+        builder.Services.AddSingleton<SubjectRepository>(s => ActivatorUtilities.CreateInstance<SubjectRepository>(s, userDBPath));
+
+
+        return builder.Build();
 	}
 }
---
+++ Models/SubjectModel.cs
@@ -0,0 +1,63 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCube.Models
+{
+    [Table("student")]
+    public class SubjectModel
+    {
+
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int SubjectID { get; set; }
+
+        [MaxLength(100)]
+        public string SubjectTitle { get; set; }
+
+        [MaxLength(100)]
+        public string SubjectCode { get; set; }
+
+        [MaxLength(255)]
+        public string SubjectDescription { get; set; }
+
+        [MaxLength(100)]
+        public int SubjectCredits { get; set; }
+
+        [MaxLength(100)]
+        public int SubjectPrice { get; set; }
+
+        [MaxLength(100)]
+        public string SubjectDate { get; set; }
+
+        [MaxLength(100)]
+        public string SubjectTime { get; set; }
+
+        [MaxLength(255)]
+        public string SubjectImage { get; set; }
+
+        [MaxLength(100)]
+        public string SubjectType { get; set; }
+
+        [MaxLength(100)]
+        public string SubjectVenue { get; set; }
+
+
+
+       // public string LecturerName { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
